6a4ef53cde35b8d759bc4e0d68536d2f0d7283ee
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2021-11-02 17:57:42 +0100

    Add api root url

--- bookmanager/api/v1/books/urls.py
+++ bookmanager/api/v1/books/urls.py
@@ -1,5 +1,6 @@
 """The `urlpatterns` list routes URLs to REST API views."""
 from django.urls import path
+from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -10,7 +11,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="BookManager API",
         default_version="v1",
-        description="Test description",
+        description="API playground",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="MIT License"),
@@ -20,6 +21,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="list")),
     path("list", BooksList.as_view(), name="list"),
     path("search", BooksSearch.as_view(), name="search"),
     path("detail/<str:pk>", BooksDetail.as_view(), name="detail"),
